10b94d77b3691b43bac20ad93ce581a34f120914
SwapnilPatil9022 <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-06-10 22:41:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+
+class Demo
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Jay Ganesh...");
+	}
+}
+
+
+
+		//1			//  2       //3
+//		import      java.lang.   *      ;
+   //4		 //5
+//class      Demo
+
+//{
+	//6         //7      //9    //10   //11              //8
+//	public     static    void    main(String          arg[])
+
+//	{
+		//12       //13     //14          //15
+//		System.     out.    println     ("Jay Ganesh...");
+
+//			}
+//		}
+
+			//javac.Demo.java
+			//java Demo    / java Demo.class   / java Demo.java
+
+
+
+
+
